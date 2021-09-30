500315bff29072cddf250f0937764c0061c0a48c
aganinalika <71232664+aganinalika@users.noreply.github.com>
2021-09-30 13:07:51 +0300

    Gasstation

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace gas_station.Controllers
+{
+  [ApiController]
+  [Route("/client")]
+  public class ClientController : ControllerBase
+  {
+
+    [HttpGet("Information")]
+    public string Information(string str)
+    {
+        return str;
+    }
+
+    [HttpPost("Making")]
+    public string Makinп(string str)
+    {
+        return str;
+    }
+
+    [HttpPost("Evaluation")]
+    public string Evaluation(string str)
+    {
+        return str;
+    }
+
+    [HttpGet("Price")]
+    public string Price(string str)
+    {
+        return str;
+    }
+
+    [HttpGet("Sale")]
+    public string Sale(string str)
+    {
+        return str;
+    }
+
+    [HttpPost("Fuel")]
+    public string Fuel(string str)
+    {
+        return "str";
+    }
+
+    [HttpPost("Loyalty")]
+    public string Loyalty(string str)
+    {
+        return str;
+    }
+
+    [HttpPost("Payment")]
+    public string Payment(string str)
+    {
+        return str;
+    }
+  }
+}
+

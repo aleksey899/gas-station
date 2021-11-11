298cfac5bf6135b93fc3c8d8a8e79de8481bedbf
Dmitry Anikeev <anikdv@gmail.com>
2021-11-11 12:43:48 +0300

    Update OrderControllerd.cs

--- Domains/OrderControllerd.cs
+++ Domains/OrderControllerd.cs
@@ -2,6 +2,6 @@
 {
     public string ID { get; set; }
     public string Order_price { get; set; }
-    public string Client { get; set; }
+    public Client Client { get; set; }
     public string Payment { get; set; }
 }

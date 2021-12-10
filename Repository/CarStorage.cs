13167e108d590df38042518d021828208bccbef3
aganinalika <71232664+aganinalika@users.noreply.github.com>
2021-12-10 12:28:27 +0300

    туц-туц

--- Repository/CarStorage.cs
+++ Repository/CarStorage.cs
@@ -6,17 +6,10 @@ namespace gas_station.Repository
     public class CarStorage
     {
         private Dictionary<int, Car> Cars { get; } = new Dictionary<int, Car>();
-        //private SqlConnection Connection { get; } = new SqlConnection("Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;");
-        //public CarStorage() => Connection.Open();
 
         public void Create(Car car)
         {
             Cars.Add(car.CarId, car);
-            //var command = Connection.CreateCommand();
-            //command.CommandText = "SELECT * FROM .....";
-            //command.ExecuteNonQuery
-            //command.ExecuteReader
-            //command.ExecuteScalar
         }
 
         public Car Read(int CarId)
